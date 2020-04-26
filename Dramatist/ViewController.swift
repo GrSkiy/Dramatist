29db39cdd8e2dde2b5b58666cf137756174a622a
GrSkiy <43372592+GrSkiy@users.noreply.github.com>
2020-04-26 17:57:35 +0300

    Initial Commit

---
+++ Dramatist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dramatist
+//
+//  Created by Gr.Skiy on 26.04.2020.
+//  Copyright © 2020 Gr.Skiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
